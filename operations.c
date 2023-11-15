b24a30becd3fc941bc9b800aa5b1a3c9e0e6a6ca
hamazoon <hamazoon@gmail.com>
2023-11-15 13:33:23 +0100

    trer

---
+++ operations.c
@@ -0,0 +1,132 @@
+#include "monty.h"
+/**
+ *  * f_pall - prints the stack
+ *   * @head: stack head
+ *    * @counter: no used
+ *     * Return: no return
+ *     */
+void f_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+		if (h == NULL)
+			return;
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+		}
+}
+/**
+ *  * f_pint - prints the top
+ *   * @head: stack head
+ *    * @counter: line_number
+ *     * Return: no return
+ *     */
+void f_pint(stack_t **head, unsigned int counter)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
+/**
+ * f_push - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ **/
+void f_push(stack_t **head, unsigned int counter)
+{
+	int n, j = 0, flag = 0;
+
+	if (bus.arg)
+		{
+			if (bus.arg[0] == '-')
+				j++;
+			for (; bus.arg[j] != '\0'; j++)
+			{
+				if (bus.arg[j] > 57 || bus.arg[j] < 48)
+																			flag = 1; }
+				if (flag == 1)
+				{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+					fclose(bus.file);
+					free(bus.content);
+					free_stack(*head);
+					exit(EXIT_FAILURE); 
+				}
+		}	
+	else
+	{ 
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head)};
+		exit(EXIT_FAILURE);
+	}
+		n = atoi(bus.arg);
+		if (bus.lifi == 0)
+			addnode(head, n);
+		else
+			addqueue(head, n);
+}
+/**
+ *
+ *f_pop - prints the top
+ * @head: stack head
+ * @counter: line_number
+ *Return: no retur
+ */
+void f_pop(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	if (*head == NULL)
+		{
+			fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);													exit(EXIT_FAILURE);
+																	}
+	h = *head;
+	*head = h->next;
+	free(h);
+}
+
+/**
+ * f_pchar - prints the char at the top of the stack,
+ * followed by a new line
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_pchar(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	h = *head;
+
+	if (!h)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);													free_stack(*head);
+		exit(EXIT_FAILURE);
+
+	}
+	if (h->n > 127 || h->n < 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+																}
+	printf("%c\n", h->n);
+}
